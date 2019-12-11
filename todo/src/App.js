6b839d2fbce7f24ee419f36176984738b8468ca7
robomantis19 <freefavors@gmail.com>
2019-12-11 11:03:16 -0500

    finished clear

--- todo/src/App.js
+++ todo/src/App.js
@@ -2,6 +2,7 @@ import React, { useReducer, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { initialState, todoReducer } from './reducers/Todo'
+import { clear } from 'sisteransi';
 function App() {
   const [todo, setTodo] = useState("")
   const [todoState, dispatch] = useReducer(todoReducer, initialState);
@@ -30,8 +31,19 @@ function App() {
     setTodo("");
   }
 
-  const clearCompleted = (id) => { 
+  const toggleCompleted = (id) => { 
     dispatch({type: "TOGGLE_TODO", payload: id})
+    // return !todoState.completed
+  }
+  const checkAll = () => { 
+    dispatch({type: "CHECKALL_COMPLETED"})
+  }
+
+  const uncheckAll = () => { 
+    dispatch({type: "UNCHECKALL_COMPLETED"})
+  }
+  const clear = () => { 
+    dispatch({type: "CLEAR"})
   }
 
   let arr = []
@@ -40,9 +52,12 @@ function App() {
   return (
     <div className="App">
       {console.log("todo", todoState)}
-      {todoState.item.map(input => {
-        return (<div key={input.id} onClick={() => clearCompleted(input.id)} style = { input.completed ? {textDecoration: `line-through darkRed` , color: 'grey'} : null}>{input.item}</div>)})}
-      {/* {todo} */}
+      
+      {todoState.todoitems.map(input => {
+        return (<div key={input.id} onClick={() => toggleCompleted(input.id)} style = { input.completed ? {textDecoration: `line-through darkRed` , color: 'grey'} : null}>{input.item}</div>)})}
+      
+     
+     
       <form onSubmit = {handleSubmit}>
         <input
         type="text"
@@ -51,6 +66,9 @@ function App() {
         onChange={handleChange}
         />
         <button>Add Todo</button>
+        <button onClick={() => checkAll() }>CheckAll</button>
+        <button onClick={() => uncheckAll()}>UnCheck All</button>
+        <button onClick={() => clear()}>Clear</button>
       </form>
     </div>
   );
--- todo/src/reducers/Todo.js
+++ todo/src/reducers/Todo.js
@@ -1,8 +1,8 @@
 export const initialState = 
 {
-    item: [{
+    todoitems: [{
 
-        item: "",
+        item: "Website Dev Stuff",
         completed: false,
         id: Date.now()
     }]
@@ -13,19 +13,59 @@ export const todoReducer = (state, action) => {
 
     switch(action.type){
         case "ADD_TODO":
-            return {item: [...state.item, action.payload]}
+            return {todoitems: [...state.todoitems, action.payload]}
         case "TOGGLE_TODO":
-            return {item: state.item.filter(input => (input.id != action.payload))}
-        case "CLEAR_COMPLETED":
+            let toggledToDos = [...state.todoitems]
+            toggledToDos.map(output => { 
+                if(output.id === action.payload){
+                    output.completed = !output.completed;
+                }
+            })
+            return {...state, todoitems: toggledToDos}
+
+
+            // return {item: state.item.map(input => {
+            //     if (input.id === action.payload){
+            //         return input.completed = !input.completed
+            //     }else{
+            //         return 
+            //     }
+            // })}
+        case "CHECKALL_COMPLETED":
             return{
-                item: state.item.map(meme => {
-                    if(meme.id === action.payload){
-                        return {...state.item, completed: !meme.completed}
+                todoitems: [...state.todoitems].filter(meme => {
+                    if(meme.completed = true){
+                        return {...state.todoitems, completed: !meme.completed}
                     }else{
-                        return meme
+                        return {...state, todoitems: [...state.todoitems]}
                     }
                 })
             }
+            
+
+        case "UNCHECKALL_COMPLETED":
+            let completedCleared = [...state.todoitems]
+            completedCleared.filter(meme => {
+                if(meme.completed === true){
+                    return meme.completed = !meme.completed
+                    // return meme.item = "";
+                }
+            });
+            return {...state, todoitems: completedCleared}
+    
+        case "CLEAR":
+            let clearAll = [...state.todoitems]
+            clearAll.filter(meme => {
+                if(meme.completed === true){
+                    // return meme.completed = !meme.completed
+                    return meme.item = "";
+                }
+            });
+            return {...state, todoitems: clearAll}
+
+        // case "CLEAR":
+        //     let clearAll = [...state.todoitems]
+
         default:
             return state;
     }
